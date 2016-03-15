53e2f64183146a2e11eb776936ef4b73dbcf8432
dolgo <dolglisiy@mail.ru>
2016-03-15 10:14:09 +0300

    Update MessageController.php

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -8,11 +8,11 @@ class MessageController extends Controller {
 
     public function getListFromDB()
     {
-        $page = Request::input('page', 0);
+        $page = Request::input('page', 1);
         $size = Request::input('size', 10);
 
 
-        return Message::skip($page * $size)->take($size)->orderBy('id')->get();
+        return Message::skip(($page - 1) * $size)->take($size)->orderBy('id')->get();
     }
 
     public function getList()
